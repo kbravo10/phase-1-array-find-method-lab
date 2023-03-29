c7fe491c0ba347453c26d4d3115afdb429082756
Kevin Bravo <kbravo1024@gmail.com>
2023-03-29 16:23:46 -0700

    complete find() lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+
+function superbowlWin(arrObj){
+    if(arrObj.find(winnner) === undefined){
+        return undefined;
+    }
+    else{
+        return arrObj.find(winnner)['year'];
+    }
+}
+
+function winnner(element, index, array){
+    return(element["result"] === "W")
+}
